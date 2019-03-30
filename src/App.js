0095ad972f939df5f14e2362860b8997bd01fa3f
ReemaGowda <reemagowdan@gmail.com>
2019-03-30 18:31:54 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Main from './Components/Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <h1>shdjs</h1>
+       <Main/>
       </div>
     );
   }
